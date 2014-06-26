f4a57e98c77931c091a661634c64a46fe329c6fd
tsb2b <tsuboshun@gmail.com>
2014-06-26 13:42:16 +0900

    Create fibo.c

---
+++ fibo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+#define NUM 65
+
+int main()
+{
+  int  i;
+  double a1[NUM];
+  double a2[NUM];
+  double a3[NUM];
+  
+  for (i=0; i<NUM; i++) {
+    if (i==0) {
+      a1[i] = 0;
+      a2[i] = 1;
+      a3[i] = 1;
+    }else{
+      a1[i] = a2[i-1];
+      a2[i] = a3[i-1];
+      a3[i] = a1[i] + a2[i];
+    }
+  }
+
+  printf("a20 = %lf\n", a1[20]);
+  printf("a30 = %lf\n", a1[30]);
+  printf("a40 = %lf\n", a1[40]);
+  printf("a50 = %lf\n", a1[50]);
+  printf("a60 = %lf\n", a1[60]);
+
+  return 0;
+}
